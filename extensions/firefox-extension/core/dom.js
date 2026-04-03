14bffe4f007eba9e6f526a51833f00c6f0819666
Laurent Le Coustumer <laurent.lecoustumer@gmail.com>
2026-04-03 16:43:33 +0200

    Tweaked firefox extension

--- extensions/firefox-extension/core/dom.js
+++ extensions/firefox-extension/core/dom.js
@@ -7,39 +7,25 @@
       this.touchedNodes = new Set();
       this.currentMode = 'asc';
       this.isTransformed = false;
+      this.observer = null;
+      this.isApplyingMutations = false;
+      this.pendingNodes = new Set();
+      this.flushScheduled = false;
     }
 
     apply(mode) {
       const textNodes = this.collectEligibleTextNodes();
-      const lang = globalScope.TidyTransform.detectDocumentLanguage();
-      let transformedCount = 0;
-
-      textNodes.forEach(node => {
-        const originalText = this.originalTextByNode.has(node)
-          ? this.originalTextByNode.get(node)
-          : node.nodeValue;
-
-        if (!this.originalTextByNode.has(node)) {
-          this.originalTextByNode.set(node, originalText);
-          this.touchedNodes.add(node);
-        }
-
-        const transformedText = globalScope.TidyTransform.tidyText(originalText, mode, lang);
-
-        if (node.nodeValue !== transformedText) {
-          node.nodeValue = transformedText;
-        }
-
-        transformedCount += 1;
-      });
+      const transformedCount = this.transformNodes(textNodes, mode);
 
       this.currentMode = mode;
       this.isTransformed = transformedCount > 0;
+      this.startObserving();
 
       return transformedCount;
     }
 
     restore() {
+      this.stopObserving();
       let restoredCount = 0;
 
       this.touchedNodes.forEach(node => {
@@ -63,7 +49,153 @@
       };
     }
 
+    transformNodes(nodes, mode) {
+      const lang = globalScope.TidyTransform.detectDocumentLanguage();
+      let transformedCount = 0;
+
+      this.withObserverPaused(() => {
+        nodes.forEach(node => {
+          if (!this.isEligibleTextNode(node)) {
+            return;
+          }
+
+          const originalText = node.nodeValue;
+          const transformedText = globalScope.TidyTransform.tidyText(originalText, mode, lang);
+
+          this.originalTextByNode.set(node, originalText);
+          this.touchedNodes.add(node);
+
+          if (node.nodeValue !== transformedText) {
+            node.nodeValue = transformedText;
+          }
+
+          transformedCount += 1;
+        });
+      });
+
+      return transformedCount;
+    }
+
+    startObserving() {
+      if (this.observer || !document.body) {
+        return;
+      }
+
+      this.observer = new MutationObserver(mutations => {
+        if (!this.isTransformed || this.isApplyingMutations) {
+          return;
+        }
+
+        mutations.forEach(mutation => {
+          if (mutation.type === 'characterData') {
+            this.queueNodeForTransform(mutation.target);
+            return;
+          }
+
+          mutation.addedNodes.forEach(node => {
+            this.collectTextNodesFromNode(node).forEach(textNode => {
+              this.queueNodeForTransform(textNode);
+            });
+          });
+        });
+      });
+
+      this.observer.observe(document.body, {
+        childList: true,
+        characterData: true,
+        subtree: true
+      });
+    }
+
+    stopObserving() {
+      if (this.observer) {
+        this.observer.disconnect();
+        this.observer = null;
+      }
+
+      this.pendingNodes.clear();
+      this.flushScheduled = false;
+    }
+
+    queueNodeForTransform(node) {
+      if (!node || node.nodeType !== Node.TEXT_NODE) {
+        return;
+      }
+
+      if (!this.isEligibleTextNode(node)) {
+        return;
+      }
+
+      this.pendingNodes.add(node);
+
+      if (!this.flushScheduled) {
+        this.flushScheduled = true;
+        window.setTimeout(() => this.flushPendingNodes(), 60);
+      }
+    }
+
+    flushPendingNodes() {
+      this.flushScheduled = false;
+
+      if (!this.isTransformed || this.pendingNodes.size === 0) {
+        this.pendingNodes.clear();
+        return;
+      }
+
+      const nodes = Array.from(this.pendingNodes);
+      this.pendingNodes.clear();
+      this.transformNodes(nodes, this.currentMode);
+    }
+
+    withObserverPaused(callback) {
+      this.isApplyingMutations = true;
+
+      try {
+        callback();
+      } finally {
+        this.isApplyingMutations = false;
+      }
+    }
+
+    collectTextNodesFromNode(node) {
+      if (!node) {
+        return [];
+      }
+
+      if (node.nodeType === Node.TEXT_NODE) {
+        return [node];
+      }
+
+      if (node.nodeType !== Node.ELEMENT_NODE || this.shouldIgnoreElement(node)) {
+        return [];
+      }
+
+      const walker = document.createTreeWalker(
+        node,
+        NodeFilter.SHOW_TEXT,
+        {
+          acceptNode: candidate => this.isEligibleTextNode(candidate)
+            ? NodeFilter.FILTER_ACCEPT
+            : NodeFilter.FILTER_REJECT
+        }
+      );
+
+      const nodes = [];
+      let currentNode = walker.nextNode();
+
+      while (currentNode) {
+        nodes.push(currentNode);
+        currentNode = walker.nextNode();
+      }
+
+      return nodes;
+    }
+
     collectEligibleTextNodes() {
+      if (!document.body) {
+        return [];
+      }
+
       const walker = document.createTreeWalker(
         document.body,
         NodeFilter.SHOW_TEXT,
